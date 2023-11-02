8687126705867c9aa3f7edce66426e9b9dce1b10
ParsaValii <Parsa.Valizadee@gmail.com>
2023-11-02 12:10:27 +0330

    Fixed ITest did not delete

--- CoffeShopApi/Program.cs
+++ CoffeShopApi/Program.cs
@@ -18,7 +18,6 @@ builder.Services.AddDbContext<CoffeShopDbContext>(
 
 // builder.Services.AddDbContext<CoffeShopDbContext>(o => o.UseNpgsql("Server=localhost;Database=coffeshopdb;Port=5432;User Id=postgres;Password=postgres"));//builder.Configuration.GetConnectionString("CoffeShopDb")
 builder.Services.AddScoped<IItemService, ItemService>();
-builder.Services.AddScoped<ITestService, TestService>();
 
 var app = builder.Build();
 
